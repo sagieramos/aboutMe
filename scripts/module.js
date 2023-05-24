3693155c06bf7d28d3549a7b255d0e028ed7ec7f
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-05-24 09:55:35 +0100

    Mobile popup completed

--- scripts/module.js
+++ scripts/module.js
@@ -13,79 +13,98 @@ const projects = [
     article: generalArticle,
     image: './assets/cards/card1.svg',
     liveLink: '#',
-    sourceLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
   },
   {
-    name: 'Multi-Post Stories',
+    name: 'Multi-Post<br>Stories',
     article: generalArticle,
     image: './assets/cards/card2.svg',
     liveLink: '#',
-    sourceLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
+  },
+  {
+    name: 'Tonic',
+    article: generalArticle,
+    image: './assets/cards/card3.svg',
+    liveLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
+  },
+  {
+    name: 'Multi-Post<br>Stories',
+    article: generalArticle,
+    image: './assets/cards/card4.svg',
+    liveLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
   },
 ];
 
 function popup(project, index) {
   const proj = project[index];
-  const str = `<section class="popup">
-<img class="popup-cancel" id="close" src="./assets/IconCancelGray.svg" alt="cancel-icon">
-<section class="card pop-card">
-    <div class="card-header">
-        <h2>${proj.name}</h2>
-        <ul class="copyright">
-            <li class="m-img">CANOPY</li>
-            <li>Back End Dev</li>
-            <li>2015</li>
-        </ul>
-    </div>
-    <img class="m-img" src="${proj.image}" alt="Tonic">
-    <article class="card-container">
-        <p class="content">
-            ${proj.article}
-        </p>
-        <ul class="tags">
-            <li><a href="#">HTML</a></li>
-            <li><a href="#">CSS</a></li>
-            <li><a href="#">JavaScript</a></li>     
-        </ul>
-        <div class="line">
-            <div></div>
+  const str = `
+    <section class="popup">
+    <img class="popup-cancel" id="close" src="./assets/IconCancelGray.svg" alt="cancel-icon">
+    <section class="card pop-card">
+        <div class="card-header">
+            <h2>${proj.name}</h2>
+            <ul class="copyright">
+                <li class="m-img">CANOPY</li>
+                <li>Back End Dev</li>
+                <li>2015</li>
+            </ul>
         </div>
-        <ul class="action">
-            <li>
-                <a class="action-btn btn" href="${proj.liveLink}">
-                    <p>See live</p>
-                    <img class="action-img" src="./assets/live.svg" alt="live">
-                </a>
-            </li>
-            <li>
-                <a class="action-btn btn" href="${proj.sourceLink}">
-                    <p>See source</p>
-                    <img class="action-img" src="./assets/social/githubBlue.svg" alt="source">
-                </a>
-            </li>
-        </ul>
-    </article>
-</section>    
-</section>`;
+        <img class="m-img" src="${proj.image}" alt="Tonic">
+        <article class="card-container">
+            <p class="content">
+                ${proj.article}
+            </p>
+            <ul class="tags">
+                <li><a href="#">HTML</a></li>
+                <li><a href="#">CSS</a></li>
+                <li><a href="#">JavaScript</a></li>     
+            </ul>
+            <div class="line">
+                <div></div>
+            </div>
+            <ul class="action">
+                <li>
+                    <a class="action-btn btn" href="${proj.liveLink}">
+                        <p>See live</p>
+                        <img class="action-img" src="./assets/live.svg" alt="live">
+                    </a>
+                </li>
+                <li>
+                    <a class="action-btn btn" href="${proj.sourceLink}">
+                        <p>See source</p>
+                        <img class="action-img" src="./assets/social/githubBlue.svg" alt="source">
+                    </a>
+                </li>
+            </ul>
+        </article>
+    </section>    
+    </section>
+`;
 
   return str;
 }
 
-let button = document.getElementsByClassName('btn-card')
-let pop = document.getElementById('cont');
-let closeBtn = "";
+const buttons = document.getElementsByClassName('btn-card');
+const pop = document.getElementById('cont');
 const main = document.getElementById('main');
-for (let i = 0; i < button.length; i = i + 1)
-button[i].addEventListener('click', () => {
-    cont.innerHTML = popup(projects, i);
-    closeBtn = document.getElementById('close');
+const header = document.getElementById('header');
+
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', () => {
+    pop.innerHTML = popup(projects, i);
     pop.style.display = 'block';
     main.style.display = 'none';
-});
+    header.style.display = 'none';
 
-closeBtn.addEventListener('click', function() {
-    pop.style.display = 'none';
+    const closeBtn = document.getElementById('close');
+
+    closeBtn.addEventListener('click', () => {
+      pop.style.display = 'none';
+      main.style.display = 'block';
+      header.style.display = 'block';
+    });
   });
-
-  
-  
+}
