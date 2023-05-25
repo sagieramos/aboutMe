6837436d8582504d6c9f3bf87ec8f065d6889383
Stanley Osagie Ramos <sagiecyber@gmail.com>
2023-05-25 06:31:27 +0100

    fixing dual visibility bug

--- scripts/module.js
+++ scripts/module.js
@@ -87,12 +87,130 @@ function popup(project, index) {
   return str;
 }
 
+const DgeneralArticle = `
+Lorem Ipsum is simply dummy text of the printing and typesetting 
+industry. Lorem Ipsum has been the industry's standard dummy text 
+ever since the 1500s, when an unknown printer took a galley of type 
+and scrambled it 1960s. Lorem Ipsum is simply dummy text of the 
+printing and typesetting industry. 
+<br> <br>
+Lorem Ipsum has been the industry's standard dummy text ever since 
+the 1500s, when an unknown printer took a galley of type and scrambled 
+it 1960s with the releorem Ipsum is simply dummy text of the printing 
+and typesetting industry. Lorem Ipsum han printer took a galley of type 
+and scrambled it 1960s with the releawn printer took a galley of type 
+and scrambled it 1960s.
+`;
+
+const Dprojects = [
+  {
+    name: 'Tonic',
+    company: 'CANOPY',
+    stack: 'Back End Dev',
+    year: '2015',
+    image: './assets/D_cards/D_card1.svg',
+    article: DgeneralArticle,
+    tech: ['html', 'CSS', 'JavaScript', 'Ruby', 'Bootstrap'],
+    liveLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
+  },
+  {
+    name: 'Multi-Post Stories',
+    company: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: '2015',
+    image: './assets/D_cards/D_card2.svg',
+    article: DgeneralArticle,
+    tech: ['html', 'Ruby on rails', 'CSS', 'JavaScript'],
+    liveLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
+  },
+  {
+    name: 'Facebook 360',
+    company: 'FACEBOOK',
+    stack: 'Full Stack Dev',
+    year: '2015',
+    image: './assets/D_cards/D_card3.svg',
+    article: DgeneralArticle,
+    tech: ['html', 'Ruby on rails', 'CSS', 'JavaScript'],
+    liveLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
+  },
+  {
+    name: 'Uber Navigation',
+    company: 'Uber',
+    stack: 'Lead Developer',
+    year: '2018',
+    image: './assets/D_cards/D_card4.svg',
+    article: DgeneralArticle,
+    tech: ['html', 'Ruby on rails', 'CSS', 'JavaScript'],
+    liveLink: '#',
+    sourceLink: 'https://github.com/sagieramos/',
+  },
+];
+
+function Dpopup(obj, index) {
+  const proj = obj[index];
+  let techLi = `<ul class="tech tags">
+    `;
+  for (let i = 0; i < proj.tech.length; i += 1) {
+    techLi += `<li><a href="#">${proj.tech[i]}</a></li>
+        `;
+  }
+  techLi += '</ul>';
+  const techUL = techLi;
+
+  const str = `
+    <section class="D-pop-card">
+    <section class="D-popup">
+   
+        <div class="D-X">
+        <div class="card-header">
+            <h2>${proj.name}</h2>
+            <ul class="copyright">
+                <li class="d-img">${proj.company}</li>
+                <li>${proj.stack}</li>
+                <li>${proj.year}</li>
+            </ul>
+        </div> 
+        <img class="D-popup-cancel" id="D-close" src="./assets/IconCancelGray.svg" alt="cancel-icon">
+        </div>
+    <img class="D-popup-img" src="${proj.image}" alt="Tonic" class="d-img">
+    <section class="D-h3and">
+        <article class="article">${proj.article}</article>
+        <div class="TnA">
+            ${techUL}
+            <div class="line">
+                <div></div>
+            </div>
+            <ul class="D-action">
+                <li>
+                    <a class="action-btn btn" href="${proj.liveLink}">
+                        <p>See live</p>
+                        <img class="action-img" src="./assets/live.svg" alt="live">
+                    </a>
+                </li>
+                <li>
+                    <a class="action-btn btn" href="${proj.sourceLink}">
+                        <p>See source</p>
+                        <img class="action-img" src="./assets/social/githubBlue.svg" alt="source">
+                    </a>
+                </li>
+            </ul>
+
+        </div>
+    </section>
+    </section>
+</section>`;
+
+  return str;
+}
+
 const buttons = document.getElementsByClassName('btn-card');
 const pop = document.getElementById('cont');
+const Dpop = document.getElementById('D-cont');
 const header = document.getElementById('header');
-const card = document.getElementsByClassName('c-hide');
-const about = document.getElementById('about');
-const contact = document.getElementsByClassName('contactme-container');
+const main = document.getElementById('main');
 
 for (let i = 0; i < buttons.length; i += 1) {
   buttons[i].addEventListener('click', () => {
@@ -103,25 +221,36 @@ for (let i = 0; i < buttons.length; i += 1) {
     const previousScrollTop = (
       window.pageYOffset || document.documentElement.scrollTop
         || document.body.scrollTop || 0);
-    for (let j = 0; j < card.length; j += 1) {
-      if (j !== i) card[j].style.display = 'none';
-    }
 
+    main.style.display = 'none';
     header.style.display = 'none';
-    about.style.display = 'none';
-    contact[0].style.display = 'none';
 
     const closeBtn = document.getElementById('close');
 
     closeBtn.addEventListener('click', () => {
       pop.style.display = 'none';
       window.scrollTo(0, previousScrollTop);
-      for (let j = 0; j < card.length; j += 1) {
-        card[j].style.display = 'block';
-      }
+      main.style.display = 'block';
       header.style.display = 'block';
-      about.style.display = 'block';
-      contact[0].style.display = 'block';
+      Dpop.style.display = 'none';
+    });
+  });
+}
+
+for (let i = 0; i < buttons.length; i += 1) {
+  buttons[i].addEventListener('click', () => {
+    Dpop.innerHTML = Dpopup(Dprojects, i);
+    Dpop.style.display = 'flex';
+    main.style.display = 'none';
+    header.style.display = 'none';
+
+    const closeBtn = document.getElementById('D-close');
+
+    closeBtn.addEventListener('click', () => {
+      Dpop.style.display = 'none';
+      main.style.display = 'block';
+      header.style.display = 'block';
+      pop.style.display = 'none';
     });
   });
 }
